d23e85e41712d6dd9629f9fd3d743e511ed8d400
Chris Hart <c.hart@afs.com>
2023-05-25 18:16:16 -0400

    Modifies functionality to only create a single sync branch and update that branch Adds reviewers/team reviews to the PR Updates dependency versions

--- dist/index.js
+++ dist/index.js
@@ -2482,66 +2482,95 @@ exports.prepareKeyValueMessage = prepareKeyValueMessage;
 
 const core = __webpack_require__(470);
 const github = __webpack_require__(469);
-const createBranch = __webpack_require__(503);
+const createOrUpdateBranch = __webpack_require__(503);
 
 async function run() {
   try {
     const sourceBranch = core.getInput("SOURCE_BRANCH", { required: true });
     const targetBranches = core.getInput("TARGET_BRANCH", { required: true });
     const githubToken = core.getInput("GITHUB_TOKEN", { required: true });
+
     const reviewers = core.getInput("REVIEWERS", { required: false });
+    const teamReviewers = core.getInput("TEAM_REVIEWERS", { required: false });
+
+    let reviewersArray = teamReviewersArray = [];
+
+    if (reviewers) {
+      reviewersArray = reviewers.split(",");
+    }
+
+    if (teamReviewers) {
+        teamReviewersArray = teamReviewers.split(",");
+    }
 
     const targetBranchesArray = targetBranches.split(",");
 
     for (let branch of targetBranchesArray) {
       console.log(`Making a pull request for ${branch} from ${sourceBranch}.`);
-      const {
-        payload: { repository },
-      } = github.context;
-
-      const octokit = new github.GitHub(githubToken);
-      //part of test
-      const { data: currentPulls } = await octokit.pulls.list({
-        owner: repository.owner.login,
-        repo: repository.name,
-      });
-      //create new branch from master branch and PR between new branch and target branch
-
       const context = github.context;
-      const newBranch = `${branch}-sync-${context.sha.slice(-4)}`;
-      await createBranch(octokit, context, newBranch);
+
+      const octokit = new github.getOctokit(githubToken);
+      //part of test
+      const { data: currentPulls } = await octokit.rest.pulls.list({
+        ...context.repo,
+      });
+      //create new branch from source branch and PR between new branch and target branch
+
+      const {
+        data: { object: { sha } }
+      } = await octokit.rest.git.getRef({
+        ref: `heads/${sourceBranch}`,
+        ...context.repo,
+      });
+      console.log(`${sourceBranch} is at ${sha}.`);
+
+      //compare diffs between branches in order to avoid creating empty pull request
+      const { data: branchDiff } = await octokit.rest.repos.compareCommitsWithBasehead({
+           ...context.repo,
+            basehead: `${branch}...${sourceBranch}`,
+      });
+
+      if (branchDiff.files.length == 0) {
+          console.log(`No need to create new PR, there are no file changes between ${sourceBranch} and ${branch}`)
+          continue;
+      }
+
+      const newBranch = `promote-to-${branch}`;
+      await createOrUpdateBranch(octokit, context.repo, sha, newBranch);
+      console.log(`Intermediate branch for PR: ${newBranch}.`);
 
       const currentPull = currentPulls.find((pull) => {
         return pull.head.ref === newBranch && pull.base.ref === branch;
       });
+      console.log(`currentPull: ${currentPull}`);
 
+      core.setOutput("PULL_REQUEST_BRANCH", newBranch);
       if (!currentPull) {
-        const { data: pullRequest } = await octokit.pulls.create({
-          owner: repository.owner.login,
-          repo: repository.name,
+        const { data: pullRequest } = await octokit.rest.pulls.create({
           head: newBranch,
           base: branch,
-          title: `Merges ${branch} into ${newBranch}`,
-          body: `Merges ${newBranch} (${branch}) into ${newBranch}`,
+          title: `Merges ${sourceBranch} into ${branch}`,
+          body: `Merges ${newBranch} (${sourceBranch}) into ${branch}`,
           draft: false,
+          ...context.repo,
         });
 
-        if(reviewers)
-        {
-          await octokit.pulls.createReviewRequest({
-            owner: repository.owner.login,
-            repo: repository.name,
-            pull_number: pullRequest.number,
-            reviewers: (reviewers || "").trim().split(/\s*,\s*/),
-          });
-        }
-
         console.log(
-          `Pull request (${pullRequest.number}) successful! You can view it here: ${pullRequest.url}.`
+          `Pull request (${pullRequest.number}) successful! You can view it here: ${pullRequest.html_url}`
         );
 
         core.setOutput("PULL_REQUEST_URL", pullRequest.url.toString());
         core.setOutput("PULL_REQUEST_NUMBER", pullRequest.number.toString());
+
+        if (reviewers || teamReviewers) {
+          await octokit.rest.pulls.requestReviewers({
+            pull_number: pullRequest.number,
+            reviewers: reviewersArray,
+            team_reviewers: teamReviewersArray,
+            ...context.repo,
+          });
+
+        }
       } else {
         console.log(
           `There is already a pull request (${currentPull.number}) to ${branch} from ${newBranch}.`,
@@ -9192,18 +9221,25 @@ exports.default = _default;
 /***/ 503:
 /***/ (function(module) {
 
-async function createBranch(octokit, context, branch) {
+async function createOrUpdateBranch(octokit, repo, sha, branch) {
   try {
-    await octokit.repos.getBranch({
-      ...context.repo,
+    await octokit.rest.repos.getBranch({
+      ...repo,
       branch,
+    });
+    // If branch already exists, update ref
+    await octokit.rest.git.updateRef({
+      ref: `heads/${branch}`,
+      sha: sha,
+      force: true,
+      ...repo,
     });
   } catch (error) {
     if (error.name === "HttpError" && error.status === 404) {
-      await octokit.git.createRef({
+      await octokit.rest.git.createRef({
         ref: `refs/heads/${branch}`,
-        sha: context.sha,
-        ...context.repo,
+        sha: sha,
+        ...repo,
       });
     } else {
       console.log("Error while creating new branch");
@@ -9212,7 +9248,7 @@ async function createBranch(octokit, context, branch) {
   }
 }
 
-module.exports = createBranch;
+module.exports = createOrUpdateBranch;
 
 
 /***/ }),
